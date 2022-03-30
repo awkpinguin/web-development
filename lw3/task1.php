d59079aea2b56f877e68893438f0e9f4555eed06
awkpinguin <nastiakaplunova@yandex.ru>
2022-03-30 08:52:34 +0300

    изменила лабу 3

--- lw3/task1.php
+++ lw3/task1.php
@@ -1,9 +1,30 @@
 <?php
-    header("Content-Type: text/plain");
-    $text = trim($_GET['text']);
-    while (strpos($text, '  ') == true)
+
+function getRequestParameter(string $key): ?string
+{
+    return $_GET[$key] ?? null;
+}
+
+$identifier=getRequestParameter('identifier');
+
+if ($identifier === null || $identifier === "")
+{
+    echo 'Данные введены неверно';
+}
+else
+{
+    $arr = str_split($identifier);
+    if (is_numeric($arr[0]))
+        echo "NO, идентификатор должен начинаться с буквы";
+    else
     {
-        $text = str_replace('  ',' ', $text);
+        for ($i = 1; $i < count($arr); ++$i)
+        {
+            if (ctype_alpha($arr[$i]) || (is_numeric($arr[$i])))
+                $answer = "YES, идентификатор верный";
+            else
+                $answer = "NO, идентификатор неверный";
+        }
+        echo $answer;
     }
-    echo $text;
-?>
+}
